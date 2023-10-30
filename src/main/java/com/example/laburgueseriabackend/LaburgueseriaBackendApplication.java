fe9b94c08a27f992567fa5d65a1610d50c385ee7
spell-26 <jsc.2612@gmail.com>
2023-10-30 00:59:48 -0500

    initial commit (conexion a base de datos ok)

---
+++ src/main/java/com/example/laburgueseriabackend/LaburgueseriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.laburgueseriabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaburgueseriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaburgueseriaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/laburgueseriabackend/LaburgueseriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laburgueseriabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaburgueseriaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
