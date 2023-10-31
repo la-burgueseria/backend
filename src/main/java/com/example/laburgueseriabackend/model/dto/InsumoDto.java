3aa964aecfffedaaad571991f9480cdb1a4cdde3
spell-26 <jsc.2612@gmail.com>
2023-10-31 10:32:41 -0500

    Se creo DTO para insumo

---
+++ src/main/java/com/example/laburgueseriabackend/model/dto/InsumoDto.java
@@ -0,0 +1,17 @@
+package com.example.laburgueseriabackend.model.dto;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Data //anotacion de lombok crea automaticament todos los metodos basicos, setters y getters
+@ToString //anotacion de lombok, genera el metodo ToString
+@Builder
+public class InsumoDto implements Serializable {
+
+    private Integer id;
+    private String nombre;
+    private Integer cantidad;
+
+}
