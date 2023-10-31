23c5d3995cf57a86e28dcbb73382ca9c785489e6
spell-26 <jsc.2612@gmail.com>
2023-10-31 10:31:31 -0500

    Se añadio paso de seguridad a traves del DTO para no dejar expuesto el modelo al response body y se añadio validacion sencilla al delete

--- src/main/java/com/example/laburgueseriabackend/controller/InsumoController.java
+++ src/main/java/com/example/laburgueseriabackend/controller/InsumoController.java
@@ -1,9 +1,16 @@
 package com.example.laburgueseriabackend.controller;
 
+import com.example.laburgueseriabackend.model.dto.InsumoDto;
 import com.example.laburgueseriabackend.model.entity.Insumo;
 import com.example.laburgueseriabackend.service.IInsumo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController //especificar que esta clase es un controller
 @RequestMapping("/api/v1") //asignacion de la ruta para ser consumido
@@ -12,24 +19,56 @@ public class InsumoController {
     @Autowired //inyeccion de dependencias del servicio
     private IInsumo insumoService;
 
-    @PostMapping("cliente") //rutaa para lanzar el evento post
-    public Insumo create(@RequestBody Insumo insumo){ //request body transforma el json enviado al objeto necesitado
-        return insumoService.save(insumo);
+    @PostMapping("insumo") //rutaa para lanzar el evento post
+    @ResponseStatus(HttpStatus.CREATED) //asignar códigos de respuesta http
+    public InsumoDto create(@RequestBody InsumoDto insumoDto){ //request body transforma el json enviado al objeto necesitado
+        Insumo insumoSave = insumoService.save(insumoDto);
+
+        return InsumoDto.builder()
+                .id(insumoSave.getId())
+                .nombre(insumoSave.getNombre())
+                .cantidad(insumoSave.getCantidad())
+                .build();
     }
 
-    @PutMapping("cliente")
-    public Insumo update(@RequestBody Insumo insumo){
-        return insumoService.save(insumo);
+    @PutMapping("insumo")
+    @ResponseStatus(HttpStatus.CREATED)
+    public InsumoDto update(@RequestBody InsumoDto insumoDto){
+        Insumo insumoUpdate = insumoService.save(insumoDto);
+
+        return InsumoDto.builder()
+                .id(insumoUpdate.getId())
+                .nombre(insumoUpdate.getNombre())
+                .cantidad(insumoUpdate.getCantidad())
+                .build();
     }
 
-    @DeleteMapping("cliente/{id}")
-    public void delete(@PathVariable Integer id){ //recibe el parametro enviado por url
-        Insumo insumoDelete = insumoService.findById(id);
-        insumoService.delete(insumoDelete);
+    @DeleteMapping("insumo/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id){ //recibe el parametro enviado por url
+    //ResponseEntity maneja toda la respuesta HTTP incluyendo el cuerpo
+    //cabecera y códigos de estado lo que permite libertad para configurar la respuesta enviada
+        Map<String, Object> response = new HashMap<>();
+        try{
+            Insumo insumoDelete = insumoService.findById(id);
+            insumoService.delete(insumoDelete);
+            return new ResponseEntity<>(insumoDelete, HttpStatus.NO_CONTENT);
+        }catch(DataAccessException exDt){
+            response.put("mensaje", exDt.getMessage());
+            response.put("insumo", null);
+
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @GetMapping("cliente/{id}")
-    public Insumo showById(@PathVariable Integer id){
-        return insumoService.findById(id);
+    @GetMapping("insumo/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public InsumoDto showById(@PathVariable Integer id){
+        Insumo insumo =  insumoService.findById(id);
+
+        return InsumoDto.builder()
+                .id(insumo.getId())
+                .nombre(insumo.getNombre())
+                .cantidad(insumo.getCantidad())
+                .build();
     }
 }
