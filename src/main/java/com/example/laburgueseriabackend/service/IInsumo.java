0426eee62edc32948349fc76a4b76b690117f662
spell-26 <jsc.2612@gmail.com>
2023-10-31 10:29:45 -0500

    Se implemento uso del DTO

--- src/main/java/com/example/laburgueseriabackend/service/IInsumo.java
+++ src/main/java/com/example/laburgueseriabackend/service/IInsumo.java
@@ -1,10 +1,11 @@
 package com.example.laburgueseriabackend.service;
 
+import com.example.laburgueseriabackend.model.dto.InsumoDto;
 import com.example.laburgueseriabackend.model.entity.Insumo;
 
 public interface IInsumo {
 
-    Insumo save(Insumo insumo);
+    Insumo save(InsumoDto insumo);
     Insumo findById(Integer id);
     void delete(Insumo insumo);
 }
