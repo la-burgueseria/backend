80c083e8caf3021731282406952b6deaaa28ce02
spell-26 <jsc.2612@gmail.com>
2024-02-05 12:49:11 -0500

    se permiten todos los origenes v4

--- src/main/java/com/example/laburgueseriabackend/config/WebConfig.java
+++ src/main/java/com/example/laburgueseriabackend/config/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*");
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 
 }
